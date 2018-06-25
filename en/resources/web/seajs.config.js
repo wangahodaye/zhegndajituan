5dccc1f82c7bd5cd62a11077e8b84c28022847fd
wangahodaye <2296542308@qq.com>
2018-06-25 23:19:52 +0800

    新增站点

---
+++ en/resources/web/seajs.config.js
@@ -0,0 +1,34 @@
+/*
+* seajs.config.pc
+*/
+// 网站根路径
+seajs.root = document.getElementById("seajsConfig") && document.getElementById("seajsConfig").getAttribute("domain") || ''; 
+
+seajs.config({
+	base: seajs.root + "/resources/modules",
+	paths: {
+		"js" : seajs.root + "/resources/web/js",
+		"lib": seajs.root + "/resources/web/lib"
+	},
+	alias: {
+		"audio"		  : "audio/audio",
+		"copy"		  : "copy/ZeroClipboard",
+		"flv"		  : "flv/flv",
+		"hook"	 	  : "hook/hook",
+		"jquery" 	  : "jquery/1/jquery.js",
+		"validform"   : "validform/validform",
+		"datepicker"  : "My97DatePicker/WdatePicker",
+		"raty"		  : "raty/raty",
+		"video"		  : "video/video",
+		"webuploader" : "webuploader/webuploader",
+		// localstorage缓存
+		"seajs-localcache" : "seajs/seajs-localcache",
+		// debug
+		"seajs-debug" : "seajs/seajs-debug"
+	},
+	preload:['manifest','seajs-localcache'],
+    localcache:{
+        timeout: 30000
+    },
+    comboExcludes: /jquery\.js/ 	// 从 combo 中排除掉 jquery.js 
+});
